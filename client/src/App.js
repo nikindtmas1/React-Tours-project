3d207cb1dc61e24f30fff9b94a333b6e357c1269
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-20 20:42:11 +0200

    Added statistics component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Service from "./components/Service/Service";
 import Agency from "./components/Agency/Agency";
+import Statistics from "./components/Statistics/Statistics";
 
 function App() {
   return (
@@ -13,36 +14,11 @@ function App() {
         <About />
         <Service />
         <Agency />
+        <Statistics />
         
         
         
         
-        <div class="statistics text-capitalize text-center font-weight-bold">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 col-12">
-                        <i class="fas fa-umbrella-beach fa-3x"></i>
-                        <h3>425</h3>
-                        <p>Tourists</p>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-12">
-                        <i class="fas fa-birthday-cake fa-3x"></i>
-                        <h3>125</h3>
-                        <p>years</p>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-12">
-                        <i class="fa fa-home fa-3x"></i>
-                        <h3>325</h3>
-                        <p>cottages</p>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-12">
-                        <i class="fas fa-glass-cheers fa-3x"></i>
-                        <h3>120</h3>
-                        <p>restaurants</p>
-                    </div>
-                </div>
-            </div>
-        </div>
         
         <div class="contact" id="contact">
             <div class="violet-overlay">
---
+++ client/src/components/Statistics/Statistics.js
@@ -0,0 +1,33 @@
+
+const Statistics = () => {
+    return (
+        <div className="statistics text-capitalize text-center font-weight-bold">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 col-12">
+                        <i className="fas fa-umbrella-beach fa-3x"></i>
+                        <h3>425</h3>
+                        <p>Tourists</p>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-12">
+                        <i className="fas fa-birthday-cake fa-3x"></i>
+                        <h3>125</h3>
+                        <p>years</p>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-12">
+                        <i className="fa fa-home fa-3x"></i>
+                        <h3>325</h3>
+                        <p>cottages</p>
+                    </div>
+                    <div className="col-lg-3 col-md-6 col-12">
+                        <i className="fas fa-glass-cheers fa-3x"></i>
+                        <h3>120</h3>
+                        <p>restaurants</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Statistics;
