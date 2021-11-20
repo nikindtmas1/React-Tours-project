e05334249dadf81a6854b5f37892e4a90ab2b8da
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-20 20:37:22 +0200

    Added service component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Navigation from "./components/Navigation/Navigation";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Service from "./components/Service/Service";
 
 function App() {
   return (
@@ -9,37 +10,9 @@ function App() {
         <Navigation />
         <Header />
         <About />
-       
+        <Service />
         
-        <div class="services" id="service">
-            <div class="container">
-                <h2 class="text-capitalize">Enjoy <span>Resort</span></h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a o</p>
-            </div>
-            <div class="small-container">
-                <div id="slideToNext" class="carousel slide" data-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img src="imgs/pic4.jpg" class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                      <img src="imgs/pic3.jpg" class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                      <img src="imgs/pic2.jpg" class="d-block w-100" alt="..." />
-                    </div>
-                  </div>
-                  <a class="carousel-control-prev" href="#slideToNext" role="button" data-slide="prev">
-                    <i class="fas fa-chevron-left fa-2x"></i>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#slideToNext" role="button" data-slide="next">
-                    <i class="fas fa-chevron-right fa-2x"></i>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
-            </div>
-        </div>
+        
         
         <div class="agency" id="agency">
             <div class="white-overlay">
---
+++ client/src/components/Service/Service.js
@@ -0,0 +1,36 @@
+
+const Service = () => {
+    return (
+        <div className="services" id="service">
+            <div className="container">
+                <h2 className="text-capitalize">Enjoy <span>Resort</span></h2>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a o</p>
+            </div>
+            <div className="small-container">
+                <div id="slideToNext" className="carousel slide" data-ride="carousel">
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                      <img src="imgs/pic4.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                      <img src="imgs/pic3.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                      <img src="imgs/pic2.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                  </div>
+                  <a className="carousel-control-prev" href="#slideToNext" role="button" data-slide="prev">
+                    <i className="fas fa-chevron-left fa-2x"></i>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a className="carousel-control-next" href="#slideToNext" role="button" data-slide="next">
+                    <i className="fas fa-chevron-right fa-2x"></i>
+                    <span className="sr-only">Next</span>
+                  </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
