ef1455ee961ddb8c400d2137d5dfc67a29226467
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-11-20 20:22:25 +0200

    Create header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,12 @@
 import Navigation from "./components/Navigation/Navigation";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
        
         <Navigation />
-        <header id="home">
-            <div class="overlay">
-                <div class="container">
-                    <div>
-                        <h1><span>Amazing</span><br />Santorini 10 Days Tour</h1>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as</p>
-                        <button><a href="#" class="text-uppercase">book now</a></button>
-                    </div>
-                </div>
-            </div>
-        </header>
+        <Header />
         
         <div class="about-us" id="about">
             <div class="small-container">
